829e582977f0763083cea089934cd828e44d144e
Kartikeya Vashisth <kartikeya.vashisth@quicken.com>
2024-01-27 01:36:02 +0530

    slider commit

--- src/main/java/seleniumsessions/SliderActions.java
+++ src/main/java/seleniumsessions/SliderActions.java
@@ -13,13 +13,13 @@ public class SliderActions {
 		driver.get("http://jqueryui.com/resources/demos/slider/default.html");
 		WebElement slider = driver.findElement(By.id("slider"));
 
-		slider.click();
+		slider.click(); //With Selenium 3.4.0 and above, it will click at the centre of the slider bar.
 		
 		int width = slider.getSize().getWidth();
 		System.out.println(width);//1264
 //		
 		Actions act = new Actions(driver);
-		//act.moveToElement(slider, -((width/2)-30), 0).click().build().perform();
+		//act.moveToElement(slider, -((width/2)-30), 0).click().build().perform(); //-((width/2)-30) negative means move to the left from centre of slider, positive > Right side.
 		act.clickAndHold(slider).moveByOffset((width/2-40), 0).build().perform();
 	
 
